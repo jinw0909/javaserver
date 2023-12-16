0941f44175b37dabb27649ebba585033f6d8eaa2
Jinwoo Chang <jinw0909@gmail.com>
2023-12-16 18:33:13 +0900

    initial commit

---
+++ src/main/java/com/jinwoo/javaserver/JavaserverApplication.java
@@ -0,0 +1,13 @@
+package com.jinwoo.javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaserverApplication.class, args);
+    }
+
+}
